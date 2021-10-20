c3413ea4d3ffd8b300158a6f33d79bab74a8eada
Matthew Bickford <bickfordm6@gmail.com>
2021-10-20 13:55:15 -0700

    backend update

---
+++ backend/routes/search.js
@@ -0,0 +1,16 @@
+"use strict";
+
+/** Routes for food/item searches. */
+
+
+const axios = require("axios");
+const express = require("express");
+
+const router = express.Router();
+
+
+router.get("/", async function (req, res, next) {
+
+});
+
+module.exports = router;
