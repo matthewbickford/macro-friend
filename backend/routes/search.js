9c3ec445a2f9530d96ff00c11f33934627df6af1
Matthew Bickford <bickfordm6@gmail.com>
2021-10-22 10:19:21 -0700

    seach endpoint update

--- backend/routes/search.js
+++ backend/routes/search.js
@@ -1,32 +1,48 @@
 "use strict";
 
 /** Routes for food/item searches. */
+
 const {X_APP_ID, X_APP_KEY} = require("../config")
 
 const axios = require("axios");
 const express = require("express");
 const router = express.Router();
 
-let config = {
+const headers = {
     headers: {
         'x-app-id': X_APP_ID,
         'x-app-key': X_APP_KEY,
         'x-remote-user-id': '0'
     }
-  }
+};
 
-router.get("/:food", async function (req, res, next) {
+
+router.get("/nutrition/:food", async function (req, res, next) {
     try {
         const food = req.params.food;
         let data = {'query': food}
         let result = await axios.post(
             "https://trackapi.nutritionix.com/v2/natural/nutrients", 
             data,
-            config )
+            headers )
         res.json(result.data);
     } catch (err) {
         return next(err)
     }
 });
+
+
+router.get("/items/:term", async function (req, res, next) {
+    try {
+        const term = req.params.term;
+        let result = await axios.get(
+            `https://trackapi.nutritionix.com/v2/search/instant?query=%22${term}%22`,
+            headers )
+        res.json(result.data);
+    } catch (err) {
+        return next(err)
+    }
+});
+
 
 module.exports = router;
